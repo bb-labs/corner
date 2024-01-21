00f15a85dfc67cf562bf42c891386350aa52b486
trumanpurnell <truman.purnell@gmail.com>
2024-01-21 14:29:39 -0700

    Add logging to figure out wtf is going on

--- corner.go
+++ corner.go
@@ -2,6 +2,7 @@ package corner
 
 import (
 	"context"
+	"log"
 
 	"google.golang.org/grpc"
 )
@@ -10,6 +11,11 @@ import (
 func AuthInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
 		var newCtx context.Context
+
+		log.Println("AuthInterceptor: ", info)
+		log.Println("AuthInterceptor: ", req)
+		log.Println("AuthInterceptor: ", handler)
+		log.Println("AuthInterceptor: ", ctx)
 
 		return handler(newCtx, req)
 	}
